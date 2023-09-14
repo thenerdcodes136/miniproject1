13de32402ceb6415b488cd01234691c234e11d3e
James <n01548367@humber.ca>
2023-09-14 10:31:00 -0400

    week2commit1, updated fileoperations

---
+++ fileoperations.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+// Read a file
+fs.readFile('example.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.error('Error reading the file:', err);
+        return;
+    }
+    console.log('File content:', data);
+});
---
+++ readme.md
@@ -0,0 +1,1 @@
+fileoperations - attempts to read a file and logs an error if it doesnt work, logs the files data if it does
